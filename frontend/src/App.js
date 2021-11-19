8bc5e10ef2eb95f0666294299b4c5af7a914b0de
anznk <anzunakayama@gmail.com>
2021-11-18 20:50:22 -0800

    make drink and food pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import GetItems from "./GetItems";
+import DrinkItems from "./DrinkItems";
+import FoodItems from "./FoodItems";
 
 function App() {
   return (
     <div className="App">
       <GetItems />
+      <DrinkItems />
+      <FoodItems />
     </div>
   );
 }
